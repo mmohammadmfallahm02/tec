c71fe8aae78e6a835998e894858d6168f677cd53
mmohammadmfallahm02 <mmohammadmfallahm02@gmail.com>
2023-01-01 21:59:20 +0330

    flutter localization added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,6 +12,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('fa', ''),//farsi
+      ],
       title: 'Flutter Demo',
       theme: ThemeData(
         // This is the theme of your application.
