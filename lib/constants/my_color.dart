0c741a961d825cfdae652a36c1ed9d008e323ca5
mmohammadmfallahm02 <mmohammadmfallahm02@gmail.com>
2023-01-02 22:11:05 +0330

    splashscreen created(spinkit added)

--- lib/constants/my_color.dart
+++ lib/constants/my_color.dart
@@ -9,6 +9,7 @@ class SolidColors {
   static const Color submitArticle = Color.fromARGB(255, 209, 209, 209);
   static const Color submitPodcast = Color.fromARGB(255, 246, 246, 246);
   static const Color subText = Color.fromARGB(255, 197, 197, 197);
+  static const Color primaryColor = Color.fromARGB(255, 68, 4, 87);
 }
 
 class GradiantColors {
---
+++ lib/splash_screen.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:tec/constants/my_color.dart';
+import 'package:tec/gen/assets.gen.dart';
+import 'package:tec/main_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    Future.delayed(Duration(seconds: 3)).then((value) => Navigator.of(context)
+        .push(MaterialPageRoute(builder: (context) => const MainScreen())));
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Center(child: Assets.images.logo.image(height: 64)),
+            SizedBox(
+              height: 32,
+            ),
+            const SpinKitFadingCube(
+              color: SolidColors.primaryColor,
+              size: 32,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
