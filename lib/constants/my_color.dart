80b07b8cc9d25a33fc669e4534ab023f1381d8e7
mmohammadmfallahm02 <mmohammadmfallahm02@gmail.com>
2023-01-02 19:31:56 +0330

    constants file created

---
+++ lib/constants/my_color.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/animation.dart';
+
+class SolidColors {
+  static const Color titleColor = Color.fromARGB(255, 40, 107, 184);
+  static const Color textColor = Color.fromARGB(255, 0, 0, 0);
+  static const Color scaffoldBg = Color.fromARGB(255, 255, 255, 255);
+  static const Color lightText = Color.fromARGB(255, 255, 255, 255);
+  static const Color selectedPodcast = Color.fromARGB(255, 255, 139, 26);
+  static const Color submitArticle = Color.fromARGB(255, 209, 209, 209);
+  static const Color submitPodcast = Color.fromARGB(255, 246, 246, 246);
+  static const Color subText = Color.fromARGB(255, 197, 197, 197);
+}
+
+class GradiantColors {
+  static const List<Color> bottomNav = [
+    Color.fromARGB(255, 25, 0, 94),
+    Color.fromARGB(255, 68, 4, 87)
+  ];
+  static const List<Color> tags = [
+    Color.fromARGB(255, 0, 0, 0),
+    Color.fromARGB(255, 63, 63, 63)
+  ];
+}
